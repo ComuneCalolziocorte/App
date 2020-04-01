c0cdcaedf79aa4b0a9ef78e9a056452eadbf517d
ComuneCalolziocorte <62992774+ComuneCalolziocorte@users.noreply.github.com>
2020-04-01 17:15:32 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -5,27 +5,15 @@ $bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
 $bot->run();
 $messages = $bot->getReceivedMessages();
 
-curl -X POST -H "Content-Type: application/json" -d '{
-  "recipient":{
-    "id":"<PSID>"
-  },
-  "messaging_type": "RESPONSE",
-  "message":{
-    "text": "Pick a color:",
-    "quick_replies":[
-      {
-        "content_type":"text",
-        "title":"Red",
-        "payload":"<POSTBACK_PAYLOAD>"
-      },{
-        "content_type":"text",
-        "title":"Green",
-        "payload":"<POSTBACK_PAYLOAD>"
-      }
-    ]
-  }
-}' "https://graph.facebook.com/v6.0/me/messages?access_token=<PAGE_ACCESS_TOKEN>"  
-
+foreach ($messages as $message)
+{
+	$recipientId = $message->senderId;
+	if($message->text)
+	{
+		$response = processRequest($message->text);
+		$bot->sendTextMessage($recipientId, $response);
+	}
+}
 
 
 
