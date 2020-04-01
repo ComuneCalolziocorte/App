9d3ecbc330a4f2f9c37a2ce839a993bb7f6d3091
ComuneCalolziocorte <62992774+ComuneCalolziocorte@users.noreply.github.com>
2020-04-01 18:31:57 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -2,38 +2,46 @@
 require_once (dirname(__FILE__) . '/config.php');
 require_once (dirname(__FILE__) . '/FacebookBot.php');
 $bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
-$bot->run();
-$messages = $bot->getReceivedMessages();
+$input = jason_decode(file_get_contents('php://input'),true);
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+$message = strlower($message);
+$url="https://graph.facebook.com/v6.0/me/messages?access_token=<PAGE_ACCESS_TOKEN>"
+$ch=curl_init($url);
 
-foreach ($messages as $message)
-{
-	$recipientId = $message->senderId;
-	if($message->text)
-	{
-		$response = processRequest($message->text);
-		$bot->sendTextMessage($recipientId, $response);
-	}
-}
+$jsonData='{
+	"recipient":{
+   	"id":"'.$sender.'"
+ 	},
+  	"messaging_type": "RESPONSE",
+  	"message":{
+    		"text": "Pick a color:",
+    		"quick_replies":[
+			 {
+			"content_type":"text",
+			"title":"Red",
+			"payload":"<POSTBACK_PAYLOAD>",
+			
+		      	},{
+			"content_type":"text",
+			"title":"Green",
+			"payload":"<POSTBACK_PAYLOAD>",
+			}
+    		]
+  		}
+};
+
+//encode array
+$jasonDataEncoded = $jsonData;
+//tell cURL that we want to send a POST request
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jasonDataEncoded);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	
+if(!empty($input['entry'][0]['messagging'][0]['message'])){
+	$result =curl_exec($ch);
+}	
+
+?>
 
 
-
-
-function processRequest($text)
-{
-	$text = trim($text);
-	$text = strtolower($text);
-	$response = "";
-	if($text=="uffici")
-	{
-		$response = "uffici e orari";
-	}
-	elseif ($text=="domanda 2")
-	{
-		$response = "risposta alla domanda 2";
-	}
-	else
-	{
-		$response = "Non capisco la domanda";
-	}
-	return $response;
-}
